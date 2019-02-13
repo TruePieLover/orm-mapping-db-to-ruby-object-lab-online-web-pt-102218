3cc386c9e9b8ff66d35716fa86f1643d47091a4a
Brandon Teich <brandonteich123@gmail.com>
2019-02-13 02:21:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,7 +49,7 @@ def self.first_student_in_grade_10
     end.first
   end
 
-  def self.all_students_in_grade_X(grade)
+def self.all_students_in_grade_X(grade)
     sql = <<-SQL
       SELECT *
       FROM students
